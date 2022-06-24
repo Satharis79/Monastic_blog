33d8d8b82277f5f562a562d65f565817c9c82864
Satharis79 <satharis@gmail.com>
2022-06-24 15:05:45 +0300

    Update urls.py

--- prayers/urls.py
+++ prayers/urls.py
@@ -3,9 +3,9 @@ from .views import prayer_list, add_prayer, prayer_edit, DeletePrayerView, handl
 
 urlpatterns = [    
     path('', prayer_list, name='home'), 
-    path('addprayer/', add_prayer, name='add_prayer'),
-    path('edit_prayer/<int:pk>', prayer_edit, name='edit_prayer'),    
-    path('delete_prayer/<int:pk>', DeletePrayerView.as_view(), name='delete_prayer'),    
+    path('add-prayer/', add_prayer, name='add_prayer'),
+    path('edit-prayer/<int:pk>', prayer_edit, name='edit_prayer'),    
+    path('delete-prayer/<int:pk>', DeletePrayerView.as_view(), name='delete_prayer'),    
     path('amen/<int:pk>', handle_amen, name='amen'),    
     path('blasphemy/<int:pk>', add_comment, name='comment'),    
 ]
